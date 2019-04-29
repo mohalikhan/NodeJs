99e1637754f8cd8d35c7305abff6d47c25768eda
Mohammad Ali <mohalikhan@hotmail.com>
2019-04-29 16:02:44 -0400

    Add overlap

--- double-booked.js
+++ double-booked.js
@@ -69,6 +69,58 @@ var overlaps = (meetings) => {
     }
 }
 
+function overlap(dateRanges){
+    var sortedRanges = dateRanges.sort((previous, current) => {
+    
+      // get the start date from previous and current
+      var previousTime = previous.start.getTime();
+      var currentTime = current.start.getTime();
+  
+      // if the previous is earlier than the current
+      if (previousTime < currentTime) {
+        return -1;
+      }
+  
+      // if the previous time is the same as the current time
+      if (previousTime === currentTime) {
+        return 0;
+      }
+  
+      // if the previous time is later than the current time
+      return 1;
+    });
+    
+    var result = sorted.reduce((result, current, idx, arr) => {
+      // get the previous range
+      if (idx === 0) { return result; }
+      var previous = arr[idx-1];
+    
+      // check for any overlap
+      var previousEnd = previous.end.getTime();
+      var currentStart = current.start.getTime();
+      var overlap = (previousEnd >= currentStart);
+    
+      // store the result
+      if (overlap) {
+        // yes, there is overlap
+        result.overlap = true;
+        // store the specific ranges that overlap
+        result.ranges.push({
+          previous: previous,
+          current: current
+        })
+      }
+     
+      return result;
+     
+       // seed the reduce  
+    }, {overlap: false, ranges: []});
+  
+  
+    // return the final results  
+    return result;
+  }
+
 // Generate some randomly meetings
 var meetings = meetings(10);
 
